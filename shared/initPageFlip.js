9f8f54916b5f95ac762ceaf3381d4df6c59b9116
Sierra Leanne Kristiaan <Railec@users.noreply.github.com>
2022-05-16 00:23:32 +0200

    Added fonts for Sylvan and Dwarvish. Added cover images for Botanical Lexicon. Added cover title to Botanical Lexicon.

--- shared/initPageFlip.js
+++ shared/initPageFlip.js
@@ -14,15 +14,18 @@ var pageFlip = null;
 
 function onFlip(e) {
 	var fakeCoverInside = document.getElementById('fake-cover-inside');
-	console.log(e.data + ' out of ' + pageFlip.getPageCount());
 	//1 is the index of the inside of the front cover.
 	//pageTotal - 3 is the index of the page before the inside of the back cover.
 	if (e.data == 1) {
 		fakeCoverInside.setAttribute('style', 'display: block; left: 600px; right: 0px;');
+		fakeCoverInside.classList.add('--right');
 	} else if (e.data > 1 && e.data < (pageFlip.getPageCount() - 3)) {
 		fakeCoverInside.setAttribute('style', 'display: block; left: 0px; right: 0px;');
+		fakeCoverInside.classList.remove('--left');
+		fakeCoverInside.classList.remove('--right');
 	} else if (e.data == (pageFlip.getPageCount() - 3)) {
 		fakeCoverInside.setAttribute('style', 'display: block; left: 0px; right: 600px;');
+		fakeCoverInside.classList.add('--left');
 	}
 }
 
@@ -30,10 +33,10 @@ function initPageFlip(bookId) {
 	//Get pages and possibly add another empty page to make sure the book is balanced.
 	//Simply here to make my life easier.
 	var pages = document.querySelectorAll('.page-container');
-	console.log(pages);
 	if (pages.length % 2) {	//If pages odd, add another at the end, inside the cover, to balance the pages.
 		var emptyPage = document.createElement('div');
-		emptyPage.setAttribute('class', 'page-container page-content');
+		emptyPage.classList.add('page-container');
+		emptyPage.classList.add('page-content');
 
 		coverInsides = document.querySelectorAll('.page-cover-inside');
 		if (coverInsides.length > 1) {
@@ -54,7 +57,8 @@ function initPageFlip(bookId) {
 		pageFlip.on('flip', onFlip);
 
 		var fakeCoverInside = document.createElement('div');
-		fakeCoverInside.setAttribute('class', 'page-cover-inside');
+		fakeCoverInside.classList.add('page-cover-inside');
+		fakeCoverInside.classList.add('--right');
 		fakeCoverInside.setAttribute('id', 'fake-cover-inside');
 		fakeCoverInside.setAttribute('style', 'display: block; left: 600px; right: 0px;');
 		book.append(fakeCoverInside);
